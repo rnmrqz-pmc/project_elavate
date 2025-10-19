50b9b1697e8f5d870523d2a71dca60bb11a23520
rnmrqz-pmc <ron-ron.marquez@powermaccenter.com>
2025-10-19 11:04:09 +0800

    comment routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -37,41 +37,41 @@ const router = createRouter({
 // ---- Navigation Guards ----
 // Handles authentication, 2FA, and role-based access before each route change
 router.beforeEach((to, from, next) => {
-  const session = useSessionStore()
-  const isLoggedIn = !!session.user
+  // const session = useSessionStore()
+  // const isLoggedIn = !!session.user
 
-  // Restrict access to protected routes
-  if (!isLoggedIn && (to.meta.requiresAuth || to.name === '2fa')) {
-    return next('/login')
-  }
+  // // Restrict access to protected routes
+  // if (!isLoggedIn && (to.meta.requiresAuth || to.name === '2fa')) {
+  //   return next('/login')
+  // }
 
-  // Redirect root path to login if not logged in
-  if (!isLoggedIn && to.path === '/') {
-    return next('/login')
-  }
+  // // Redirect root path to login if not logged in
+  // if (!isLoggedIn && to.path === '/') {
+  //   return next('/login')
+  // }
 
-  // Manage 2FA and role-based redirection
-  if (isLoggedIn) {
-    // 2FA is complete, but user is trying to access 2FA page
-    if (session.auth === 'true' && to.name === '2fa') {
-      return next('/')
-    }
+  // // Manage 2FA and role-based redirection
+  // if (isLoggedIn) {
+  //   // 2FA is complete, but user is trying to access 2FA page
+  //   if (session.auth === 'true' && to.name === '2fa') {
+  //     return next('/')
+  //   }
 
-    // 2FA not complete, redirect to 2FA page
-    if (session.auth != 'true' && to.name !== '2fa') {
-      return next({ name: '2fa' })
-    }
+  //   // 2FA not complete, redirect to 2FA page
+  //   if (session.auth != 'true' && to.name !== '2fa') {
+  //     return next({ name: '2fa' })
+  //   }
 
-    // Redirect from login/password reset/root to role-based route
-    if (to.path === '/login' || to.name === 'PasswordReset' || to.path === '/') {
-      return next(`/${session.user.role}`)
-    }
+  //   // Redirect from login/password reset/root to role-based route
+  //   if (to.path === '/login' || to.name === 'PasswordReset' || to.path === '/') {
+  //     return next(`/${session.user.role}`)
+  //   }
 
-    // Prevent access to other roles’ routes
-    if (to.meta.requiresAuth && to.meta.role && to.meta.role !== session.user.role) {
-      return next('/')
-    }
-  }
+  //   // Prevent access to other roles’ routes
+  //   if (to.meta.requiresAuth && to.meta.role && to.meta.role !== session.user.role) {
+  //     return next('/')
+  //   }
+  // }
 
   // Allow navigation to proceed
   next()
